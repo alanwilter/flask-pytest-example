e0bec610f838b8837710d26dd42b06f8e3e918e2
Alan Silva <alanwilter@gmail.com>
2020-08-19 22:56:17 +0200

    updated fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,9 @@ pytest_plugins = ("tests.fix_api",)
 
 from process_tests import TestProcess
 from process_tests import wait_for_strings
-@pytest.yield_fixture
-def app_server(scope='session'):
+
+@pytest.fixture(scope="session")
+def app_server():
     with TestProcess('python', 'app.py') as app_server:
         wait_for_strings(app_server.read, 10, "Running")
         yield app_server
